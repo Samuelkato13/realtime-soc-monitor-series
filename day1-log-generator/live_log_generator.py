30a57bba12b29f180b28d88b7d8ef2eb7b9ccf57
Samuelkato13 <144229864+Samuelkato13@users.noreply.github.com>
2026-03-09 20:09:54 +0300

    Implement live log generation for security events
    Added functionality to generate security logs with timestamps, events, users, and IPs.

--- day1-log-generator/live_log_generator.py
+++ day1-log-generator/live_log_generator.py
@@ -1,1 +1,37 @@
+import csv
+import random
+import time
+from datetime import datetime
 
+LOG_FILE = "security_logs.csv"
+
+users = ["admin","john","lucy","samuel"]
+events = ["login_success","login_failed","file_access"]
+ips = [
+"192.168.1.10",
+"192.168.1.15",
+"10.0.0.5",
+"172.16.0.3",
+"45.77.23.12"
+]
+
+with open(LOG_FILE,"w",newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["timestamp","event","user","ip"])
+
+print("Starting log generation...\n")
+
+while True:
+
+    timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    event=random.choice(events)
+    user=random.choice(users)
+    ip=random.choice(ips)
+
+    with open(LOG_FILE,"a",newline="") as file:
+        writer=csv.writer(file)
+        writer.writerow([timestamp,event,user,ip])
+
+    print(timestamp,event,user,ip)
+
+    time.sleep(random.randint(1,3))
