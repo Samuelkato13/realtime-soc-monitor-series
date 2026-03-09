eca528beac774dbebe7b5aae520b783c36fb160b
Samuelkato13 <144229864+Samuelkato13@users.noreply.github.com>
2026-03-09 21:04:12 +0300

    Implement threat detection system for login failures

---
+++ day3-threat-detection/detection_monitor.py
@@ -0,0 +1,42 @@
+import csv
+import time
+from collections import defaultdict
+
+LOG_FILE="../day1-log-generator/security_logs.csv"
+
+failed_attempts=defaultdict(int)
+
+THRESHOLD=5
+
+print("Threat detection system running...\n")
+
+with open(LOG_FILE,"r") as file:
+    reader=csv.reader(file)
+    next(reader)
+    pointer=sum(1 for row in reader)
+
+while True:
+
+    with open(LOG_FILE,"r") as file:
+        reader=list(csv.reader(file))
+
+    new_logs=reader[pointer:]
+    pointer=len(reader)
+
+    for log in new_logs:
+
+        timestamp,event,user,ip=log
+
+        if event=="login_failed":
+
+            failed_attempts[ip]+=1
+
+            if failed_attempts[ip]>=THRESHOLD:
+
+                print("\nALERT")
+                print("Possible brute force attack detected")
+                print("IP:",ip)
+                print("Failed Attempts:",failed_attempts[ip])
+                print()
+
+    time.sleep(1)
