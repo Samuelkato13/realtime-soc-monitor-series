8066a2085a6ac19b778be687408576fd13dd2f94
Samuelkato13 <144229864+Samuelkato13@users.noreply.github.com>
2026-03-09 20:35:13 +0300

    Add real-time log monitoring functionality
    Implement real-time log monitoring from a CSV file.

--- day2-realtime-monitor/realtime_monitor.py
+++ day2-realtime-monitor/realtime_monitor.py
@@ -1,1 +1,24 @@
+import csv
+import time
 
+LOG_FILE="../day1-log-generator/security_logs.csv"
+
+print("Real-time log monitoring started...\n")
+
+with open(LOG_FILE,"r") as file:
+    reader=csv.reader(file)
+    next(reader)
+    pointer=sum(1 for row in reader)
+
+while True:
+
+    with open(LOG_FILE,"r") as file:
+        reader=list(csv.reader(file))
+
+    new_logs=reader[pointer:]
+    pointer=len(reader)
+
+    for log in new_logs:
+        print("New Log:",log)
+
+    time.sleep(1)
